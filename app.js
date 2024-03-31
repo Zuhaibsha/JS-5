c792208d9a92ca743d45367d3062613f20c32faa
Zuhaibsha <zuhaibshah709@gmail.com>
2024-04-01 02:31:00 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,118 @@
+// Question 1
+// var multidimensionalArray = [];
+
+// multidimensionalArray.push([]);
+// multidimensionalArray.push([]);
+
+// multidimensionalArray[0].push(1);
+// multidimensionalArray[0].push(2);
+// multidimensionalArray[1].push(3);
+// multidimensionalArray[1].push(4);
+
+// document.write(multidimensionalArray); 
+
+
+// Question 2
+//  var matrix=[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+//  ]
+
+//  document.write(matrix + '</br>')
+
+
+// Question 3
+
+// for(var i=0;i<=10;i++){
+//  document.write([i] +'</br>')   
+// }
+
+// Queston 4
+// var user=prompt("enter number")
+// var input=prompt("enter number for loop")
+
+// for(var i=1;i<=input;i++){
+//     document.write(user+" x " +i+ "="+user*i+"<br>" )
+// }
+
+
+// Question 5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// // Using a for loop to print each item of the array
+// for (var i = 0; i < fruits.length; i++) {
+//   document.write(fruits[i]+'</br>');
+// }
+
+// Question 6
+// document.write("<h1>counting</h1></br>")
+// for(var i=1;i<=15;i++){
+// document.write( [i]+" ")
+// }
+// document.write("<h1>Reverse</h1></br>")
+// for(var i=10;i>0;i--){
+
+// document.write([i]+" ")
+// }
+
+// document.write("<h1>Even</h1></br>")
+// for(var i=0;i<=10;i=i+2){
+
+//     document.write([i]+" ")
+//     }
+
+//     document.write("<h1>ODD</h1></br>")
+// for(var i=1;i<=10;i=i+2){
+
+//     document.write([i]+" ")
+//     }
+
+
+//     document.write("<h1>series</h1></br>")
+// for(var i=2;i<=20;i=i+2){
+
+//     document.write([i]+'K'+" ")
+//     }
+    
+    
+// Question 7
+// var item=['cake','pastry','samosa']
+// var user=prompt("Enter Item you want").toLowerCase();
+// var match =false
+// for(var i=0;i<item.length;i++){
+// if(user==item[i].toLowerCase()){
+//     match=true
+// document.write(`${user} found`)
+// break;
+// }
+
+// }
+// if(match==false){
+// document.write(user+"not found")
+// }    
+
+
+
+// //Question 8
+// var A = [24, 53, 78, 91, 12];
+// var largestNumber = Math.max(...A);
+// document.write('<h1>ARRAY ITEM :</h1>' +A+'</br>')
+// document.write("The largest number in the array is: " + largestNumber);
+
+
+// Question 9
+// var A = [24, 53, 78, 91, 12];
+// var largestNumber = Math.min(...A);
+// document.write('<h1>ARRAY ITEM :</h1>' +A+'</br>')
+// document.write("The smallest number in the array is: " + largestNumber);
+
+
+// Question 10
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         document.write(i+" ");
+//     }
+// }
+
+
